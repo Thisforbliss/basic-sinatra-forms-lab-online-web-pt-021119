5a8513c84e08b66a99f8b5628ba08ea5dd273c0d
Malcolm Leake <echelon1089@gmail.com>
2019-06-24 21:07:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,15 +8,19 @@ get '/newteam' do
 end
 
 post '/newteam' do
+  #binding.pry
 @name = params[:name]
-@coach = params [:coach]
+@coach = params[:coach]
 @pg = params[:pg]
 @sg = params[:sg]
 @sf = params[:sf]
 @pf = params[:pf]
 @c = params[:c]
+#binding.pry
 erb :team
 #binding.pry
 end
+
+
 
 end
